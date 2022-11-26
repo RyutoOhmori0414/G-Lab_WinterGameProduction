4ae60ba0166ee67e267d4d1913bfd564d6747a5c
KND9908 <jikumaru954@gmail.com>
2022-11-26 22:57:08 +0900

    とりあえずフェードアウト実装 Todo:Dotween用いて実装

--- Assets/mizuta/script/btnpush.cs
+++ Assets/mizuta/script/btnpush.cs
@@ -2,14 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class btnpush : MonoBehaviour
 {
     [SerializeField]
+    GameObject FadeOutScreen;
+
+    [SerializeField]
     string gotoscene = "";
+
+    [SerializeField]
+    int fadeouttime = 120; //フェードアウトアニメーションのフレーム秒数
     // Start is called before the first frame update
    public void chgscene()
     {
+        StartCoroutine(FadeOutCall());
+    }
+
+    IEnumerator FadeOutCall()
+    {
+        FadeOutScreen.GetComponent<Image>().enabled = true;
+        while (FadeOutScreen.GetComponent<Image>().color.a < 1.0f)
+        {
+            FadeOutScreen.GetComponent<Image>().color += new Color(0, 0, 0, 1.0f / fadeouttime); //Imageのカラーを変更
+            yield return null;
+        }
         SceneManager.LoadScene(gotoscene);
+        yield break;
     }
 }
