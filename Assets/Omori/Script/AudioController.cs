e99f4e7dd911f0d3dfaa078d2ba9648633f0d212
RyutoOhmori <ryutoomori0414@gmail.com>
2022-12-05 23:25:34 +0900

    [fix] 音楽関係修正途中

--- Assets/Omori/Script/AudioController.cs
+++ Assets/Omori/Script/AudioController.cs
@@ -1,15 +1,40 @@
 using CriWare;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioController : MonoBehaviour
 {
-    CriAtomSource _atomSource;
+    [Header("SE")]
+    [SerializeField] CriAtomSource _attack;
+    [SerializeField] CriAtomSource _buffItem;
+    [SerializeField] CriAtomSource _pushButton;
+    [SerializeField] CriAtomSource _getCandy;
+    [SerializeField] CriAtomSource _damage;
+    [SerializeField] CriAtomSource _reload;
+    [SerializeField] CriAtomSource _run;
+    [SerializeField] CriAtomSource _stan;
+    [Header("ME")]
+    [SerializeField] CriAtomSource _win;
+    [SerializeField] CriAtomSource _lose;
 
-    private void Start()
+    public void Play(AudioName audioName)
     {
-        _atomSource = FindObjectOfType<CriAtomSource>();
-        _atomSource.Play("BGM");
+
+    }
+
+    public enum AudioName
+    {
+        Attack,
+        Buff,
+        PushButton,
+        GetCandy,
+        Damage,
+        Reload,
+        Run,
+        Stan,
+        Win,
+        lose
     }
 }
